d1ec621eb340e81064c3912a80b65aa3759b2292
Guitton Marceau <marceau.guitton@etudiant.univ-rennes.fr>
2024-01-11 14:09:06 +0100

    11/01/24

--- message.py
+++ message.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from datetime import datetime
 from typing import AnyStr, Protocol
 from enum import IntEnum
+import struct
+
+
 
 message = tuple[int, datetime, int, AnyStr]
 
@@ -57,6 +60,27 @@ class Message(Protocol):
         ...
 
     def encode(self) -> bytes:
-        def ConnectRequest(userid : bytes, lenghtID : bytes, lenghtPWD : bytes):
-            """self.code.CONNECT_REQUEST"""
-        
+        def ConnectRequest(userid : bytes, lenghtID : bytes, lenghtPWD : bytes, password : bytes):
+            
+            struct.pack(fmt :str, userid, password)-> bytes
+
+
+"""
+str(userid).encode("utf-8")
+str(password).encode("utf-8")
+"""
+
+
+"""encoded = bytes(fonction, 'utf-8')"""
+            
+"""self.code.CONNECT_REQUEST"""
+
+"""size = 5
+
+arr = bytes(size)
+
+print(arr)
+
+résultat: b'\x00\x00\x00\x00\x00'
+
+"""
