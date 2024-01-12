aa6b2de798ac5648ba39726d60843314481b6d0a
AllanMinSoc <allan_gonin@icloud.com>
2024-01-12 10:38:59 +0100

    premiere tentative de tls

--- connection.py
+++ connection.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from types import TracebackType
 
-from socket import socket
+from socket import socket, SOCK_STREAM
+import ssl
 
 
 class Connection:
@@ -60,11 +61,13 @@ class Connection:
 class Listener:
 
     my_socket: socket
+    context: ssl.SSLContext
     list_of_accepted: list[tuple[str, int]]
 
     def __init__(self, socket: socket) -> None:
         self.my_socket = socket
         self.list_of_accepted = []
+        self.context = ssl.create_default_context()
 
     def __enter__(self) -> Listener:
         return self
@@ -82,9 +85,11 @@ class Listener:
             Connection: Connection accepted.
             
         """
+        
         socket_accepted, address = self.my_socket.accept()
+        ssl_socket = self.context.wrap_socket(socket_accepted, server_side=True)
         self.list_of_accepted.append(address)
-        return Connection(socket_accepted)
+        return Connection(ssl_socket)
 
     def last_accepted(self) -> tuple[str, int] | None:
         return self.list_of_accepted[-1] if self.list_of_accepted else None
@@ -99,23 +104,34 @@ class Listener:
 class Server:
 
     def __init__(self, certfile: str, keyfile: str | None = None, password: str | None = None) -> None:
-        ...
+        
+        self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        self.context.load_cert_chain(certfile=certfile, keyfile=keyfile, password=password)
 
     def listen(self, address: tuple[str, int]) -> Listener:
         """Listen for connections made to the socket of the server. See `socket.bind` and `socket.listen`."""
         
-        new_socket = socket()
+        new_socket = self.context.wrap_socket(socket(type=SOCK_STREAM),
+                                                server_side=True # server side
+                                                )
         new_socket.bind(address)
         new_socket.listen()
+
         return Listener(new_socket)
 
 
 class Client:
 
     def __init__(self) -> None:
-        ...
+        
+        self.context = ssl.create_default_context()
 
     def connect(self, address: tuple[str, int]) -> Connection:
-        new_socket = socket()
+
+        
+        new_socket = self.context.wrap_socket(socket(type=SOCK_STREAM),
+                                                server_side=False, # client side
+                                                server_hostname=address[0]
+                                                )
         new_socket.connect(address)
         return Connection(new_socket)
